ce4f156ebc157535969f7a49ae1184a7a2a1749c
ergincetn01 <ergincetn01@gmail.com>
2023-09-23 23:56:58 +0300

    complete app with redux and react-ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { reducers } from "./reducers";
+import { App } from "./components/App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+export const store = configureStore({
+  reducer: reducers,
+  // middleware: applyMiddleware(thunk)
+});
+
+// const store= createStore(reducers, applyMiddleware(thunk));
+
+// interface AppProps {
+//   color: string;
+// }
+
+// interface AppState {
+//   counter: number;
+// }
+
+const element = document.getElementById("root") as HTMLElement;
+const root= ReactDOM.createRoot(element);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <App  />
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
